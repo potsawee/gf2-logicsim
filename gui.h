8731d7440b93d12bb11d9735c325f67f7c82fb1a
Shutong Feng <shutong.feng@gmail.com>
2018-05-22 15:10:56 +0100

    Corresponding changes to button and menu bar

--- gui.h
+++ gui.h
@@ -12,7 +12,8 @@
 enum { 
   MY_SPINCNTRL_ID = wxID_HIGHEST + 1,
   MY_TEXTCTRL_ID,
-  MY_BUTTON_ID,
+  MY_BUTTON_ID1,
+  MY_BUTTON_ID2,
 }; // widget identifiers
 
 class MyGLCanvas;
@@ -33,7 +34,8 @@ class MyFrame: public wxFrame
   void runnetwork(int ncycles);           // function to run the logic network
   void OnExit(wxCommandEvent& event);     // event handler for exit menu item
   void OnAbout(wxCommandEvent& event);    // event handler for about menu item
-  void OnButton(wxCommandEvent& event);   // event handler for push button
+  void OnButton1(wxCommandEvent& event);   // event handler for push button
+  void OnButton2(wxCommandEvent& event);
   void OnSpin(wxSpinEvent& event);        // event handler for spin control
   void OnText(wxCommandEvent& event);     // event handler for text entry field
   DECLARE_EVENT_TABLE()

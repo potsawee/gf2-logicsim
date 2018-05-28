701e3dc55a9ae18779a5eb4ba4be32c3867b971d
Shutong Feng <shutong.feng@gmail.com>
2018-05-28 15:05:01 +0100

    Changed logsim file for gui

--- logsim.cc
+++ logsim.cc
@@ -15,20 +15,13 @@ IMPLEMENT_APP(MyApp)
 bool MyApp::OnInit()
   // This function is automatically called when the application starts
 {
-  if (argc != 2) { // check we have one command line argument
-    wcout << "Usage:      " << argv[0] << " [filename]" << endl;
-    exit(1);
-  }
 
   // Construct the six classes required by the innards of the logic simulator
   nmz = new names();
   netz = new network(nmz);
   dmz = new devices(nmz, netz);
   mmz = new monitor(nmz, netz);
-  smz = new scanner(nmz, wxString(argv[1]).mb_str());
-  pmz = new parser(netz, dmz, mmz, smz, nmz);
 
-  if (pmz->readin ()) { // check the logic file parsed correctly
 #ifdef USE_GUI
     // glutInit cannot cope with Unicode command line arguments, so we pass
     // it some fake ASCII ones instead
@@ -38,10 +31,18 @@ bool MyApp::OnInit()
     frame->Show(true);
     return(true); // enter the GUI event loop
 #else
+  if (argc != 2) { // check we have one command line argument
+    wcout << "Usage:      " << argv[0] << " [filename]" << endl;
+    exit(1);
+  }
+  smz = new scanner(nmz, wxString(argv[1]).mb_str());
+  pmz = new parser(netz, dmz, mmz, smz, nmz);
+  if (pmz->readin ()) { // check the logic file parsed correctly
     // Construct the text-based interface
     userint umz(nmz, dmz, mmz);
     umz.userinterface();
-#endif /* USE_GUI */
   }
+#endif /* USE_GUI */
+
   return(false); // exit the application
 }
